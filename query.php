92808122aab4c7ae00dd430f10ba2f164c6fb649
Mykhailo Kulinich <57287929+ihslimn@users.noreply.github.com>
2023-04-25 12:53:14 +0300

    UPD: better processing of Reload type filters

--- query.php
+++ query.php
@@ -19,107 +19,41 @@ class Filters_Query {
 
 		add_filter( 'jet-smart-filters/query/meta-query-row', array( $this, 'clear_meta_query' ) );
 
-		add_filter( 'jet-smart-filters/query/final-query', array( $this, 'modify_query' ), 999 );
+		add_filter( 'jet-smart-filters/query/vars', array( $this, 'remove_missing_vars' ) );
 
 		add_filter( 'jet-smart-filters/query/request', array( $this, 'check_request' ), 0, 2 );
 
 	}
 
-	public function modify_query( $query ) {
-		
-		$_vars = array(
-			'p',
-		);
-		
-		foreach ( $this->missing_vars as $var ) {
-			if ( in_array( $var, $_vars ) ) {
-				unset( $query[ $var ] );
-			}
-		}
-
-		return $query;
-
-	}
-
-	public function set_items_per_page( $key, $value = false ) {
-
-		$params = explode( '::', $key );
-
-		$suffix = $params[1] ?? '';
-
-		$storage_type = $params[2] ?? 'session';
-
-		if ( ! $value ) {
-			
-			$args = explode( ':', $params[2] ?? array() );
-
-			$storage_type = $args[0] ?? 'session';
-
-			$value = $args[1] ?? false;
-
-		}
-
-		if ( ! $value ) {
-			return;
-		}
-
-		Storage::set( 'items_per_page', $value, $suffix, $storage_type );
-
-	}
-
 	public function check_request( $request, $query_manager ) {
 
-		$vars = $query_manager->query_vars();
+		$vars = array( 
+			'sort', 
+			'alphabet',
+			'p',
+			'author',
+			'post_type',
+		);
 
 		$present_vars = array();
 
 		if ( $query_manager->is_ajax_filter() ) {
-
-			foreach ( $request['query'] as $key => $value ) {
-
-				if ( in_array( $key, $vars ) ) {
-					$present_vars[] = $key;
-				}
-
-				if ( $this->is_item_to_process( $key ) ) {
-
-					$this->set_items_per_page( $key, $value );
-					
-				}
-
-			}
-
+			$data = $request['query'];
 		} else {
+			$data = $request;
+		}
 
-			foreach ( $request as $key => $value ) {
+		foreach ( $data as $key => $value ) {
 
-				if ( in_array( $key, $vars ) ) {
-					$present_vars[] = $key;
-				}
-
+			if ( in_array( $key, $vars ) ) {
+				$present_vars[] = $key;
 			}
 
-			$meta = $request[ 'meta' ] ?? false;
+			if ( $this->is_item_to_process( $key ) ) {
 
-			if ( ! $meta ) {
-				return $request;
+				$this->set_items_per_page( $key, $value );
+				
 			}
-
-			$meta = explode( ';', $meta );
-
-			foreach ( $meta as $key => $item ) {
-
-				if ( $this->is_item_to_process( $item ) ) {
-
-					$this->set_items_per_page( $item );
-
-					unset( $meta[ $key ] );
-
-				}
-
-			}
-
-			$request['meta'] = implode( ';', $meta );
 
 		}
 
@@ -129,11 +63,59 @@ class Filters_Query {
 
 	}
 
+	public function remove_missing_vars( $vars ) {
+				
+		foreach ( $vars as $index => $var ) {
+			if ( in_array( $var, $this->missing_vars ) ) {
+				unset( $vars[$index] );
+			}
+		}
+
+		return $vars;
+
+	}
+
+	public function get_params_from_key( $key ) {
+		
+		$result = array();
+
+		$params = explode( '__', $key );
+
+		$result['suffix'] = $params[1] ?? '';
+
+		$result['storage_type'] = $params[2] ?? 'session';
+
+		return $result;
+
+	}
+
+	public function set_items_per_page( $key, $value = false ) {
+
+		if ( ! $value ) {
+			
+			$args = explode( ':', $key ?? '' );
+
+			$key = $args[0];
+
+			$value = $args[1] ?? false;
+
+		}
+
+		if ( ! $value ) {
+			return;
+		}
+
+		$params = $this->get_params_from_key( $key );
+		
+		Storage::set( 'items_per_page', $value, $params['suffix'], $params['storage_type'] );
+
+	}
+
 	public function preset_value( $args, $filter_instance ) {
 		
 		if ( false !== strpos( $args['query_var'], Plugin::$key ) ) {
 
-			$params = explode( '::', $args['query_var'] );
+			$params = explode( '__', $args['query_var'] );
 
 			$key = 'items_per_page';
 
@@ -160,8 +142,8 @@ class Filters_Query {
 		return $row;
 	}
 
-	public function is_item_to_process( $item ) {
-		return false !== strpos( $item, Plugin::$key );
+	public function is_item_to_process( $key ) {
+		return false !== strpos( $key, Plugin::$key );
 	}
 
 }
